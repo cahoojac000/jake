07b76a133ba54e0f974b53ee7843299d83538540
cahoojac000 <jacobbcahoon@gmail.com>
2017-06-22 14:19:45 +0000

    implemented image defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,16 +3,27 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
   end
   
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+  
   def new
     @portfolio_item = Portfolio.new
   end
   
-  def edit
-    @portfolio_item = Portfolio.find(params[:id])
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
   
-  def angular
-    @angular_portfolio_items = Portfolio.angular
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
   end
   
   def update
@@ -40,17 +51,6 @@ class PortfoliosController < ApplicationController
     # Redirects
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
-    end
-  end
-  
-  def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-      else
-        format.html { render :new }
-      end
     end
   end
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,16 @@ class Portfolio < ApplicationRecord
   end
   
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  
+  after_initialize :set_defaults
+  
+  # ||= is the same as doing
+  # if self.main_image == nil
+  #   slef.main_image = "a;sldkfajsdlfj"
+  # end
+  
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
